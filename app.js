c04685f3bbcfeabd80ccf13da8a3badc252fe8d5
Niels Koster <n.koster@portavita.eu>
2018-08-06 00:56:09 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,28 @@
+const
+    express = require('express'),
+    app = express(),
+    server = app.listen(9999),
+    io = require("socket.io")(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+	res.render('index')
+});
+
+io.on('connection', (socket) => {
+    socket.username = "Anonymous";
+    
+    socket.on('change_username', (data) => {
+        console.log(`user "${socket.username}" ==> "${data.username}"`);
+        socket.username = data.username
+    });
+
+    socket.on('new_message', (data) => {
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    });
+
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
+});
---
+++ public/chat.js
@@ -0,0 +1,45 @@
+$(function() {
+
+const
+    socket = io.connect('http://localhost:9999')
+    message = $("#message"),
+    username = $("#username"),
+    send_message = $("#send_message"),
+    send_username = $("#send_username"),
+    chatroom = $("#chatroom"),
+    feedback = $("#feedback");
+
+send_message.click(function(){
+    socket.emit('new_message', {message : message.val()})
+});
+
+socket.on("new_message", (data) => {
+    feedback.html('');
+    message.val('');
+    chatroom.append("<p class='message'>" + data.username + ": " + data.message + "</p>")
+});
+
+send_username.click(function(){
+    socket.emit('change_username', {username : username.val()})
+});
+
+message.bind("keypress", () => {
+    socket.emit('typing')
+});
+
+message.bind("keyup", (event) => {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        console.log('Enter pressed');
+        socket.emit('new_message', {message : message.val()})
+    }
+});
+
+socket.on('typing', (data) => {
+    feedback.html("<p><i>" + data.username + " is typing..." + "</i></p>");
+    setTimeout( () => {
+        feedback.html('')
+    }, 5000)
+});
+
+});

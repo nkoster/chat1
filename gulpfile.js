03986b13a79e873393f26c5bccf4461408de9319
Niels Koster <n.koster@portavita.eu>
2018-08-23 20:52:39 +0200

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,6 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var gutil = require('gulp-util');
 
-// gulp.task('js', function() {
-//   gulp.src(['js/chat.js', 'js/drag.js'])
-//     .pipe(concat('chat.js'))
-//     .pipe(uglify())
-//     .pipe(gulp.dest('public/'))
-// });
-
-// gulp.task('watch:js', ['js'], function() {
-//   gulp.watch('js/**/*.js', ['js'])
-// });
-
 gulp.task('concat', function() {
 return gulp.src(['./js/jquery.min.js', './js/socket.io.js', './js/chat.js', './js/drag.js'])
     .pipe(concat('chat.js'))
